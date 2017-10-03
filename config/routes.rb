5ca6997ea14a564754b39b89c8f65668525d440f
jonaselan <jonas.elan@gmail.com>
2017-10-03 15:13:25 -0300

    Create minimal authentication for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}
+  root to: "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
