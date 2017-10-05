a79192e5f91e9e818639e89e31abab6f5864ac07
jonaselan <jonas.elan@gmail.com>
2017-10-05 19:03:10 -0300

    Add internalization (i18n)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_locale
 
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, notice: exception.message
@@ -18,4 +19,15 @@ class ApplicationController < ActionController::Base
     # [[name, id]]
     @languages = Language.pluck(:name, :id)
   end
+
+  private
+
+  def set_locale
+    I18n.locale = params[:locale] if params[:locale].present?
+  end
+
+  def default_url_options(options = {})
+    {locale: I18n.locale}
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
-  root to: "home#index"
+  scope ":locale", locale: /#{I18n.available_locales.join("|")}/ do
 
-  # authentication
-  devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}
+    root to: "home#index"
 
-  resources :users, only: [:show, :edit, :update]
-  resources :rooms, except: [:show]
+    # authentication
+    devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}
+
+    resources :users, only: [:show, :edit, :update]
+    resources :rooms, except: [:show]
+  end
+
+  get '*path', to: redirect("/#{I18n.default_locale}/%{path}")
+  get '', to: redirect("/#{I18n.default_locale}")
 end
