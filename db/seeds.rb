d4a02450041456c8a354168fdb3489c8899b2bf2
jonaselan <jonas.elan@gmail.com>
2017-10-05 17:16:35 -0300

    Create seeds for application

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+ll = Language.create(name: 'portugues', shortcode: 'pt-br')
+Language.create(name: 'inglês', shortcode: 'en')
+
+uu = User.create(email: 'email@email.com', username: 'user1', password: 123456)
+
+Room.create(link: 'http://link.com', level: 'básico', language_id: ll, user_id: uu)
