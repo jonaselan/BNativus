d4f10ae8988567ed21734fa6f0f083437a6fc7f4
jonaselan <jonas.elan@gmail.com>
2017-10-07 10:49:46 -0300

    Fix issue with seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,4 +3,4 @@ Language.create(name: 'language_en', shortcode: 'en')
 
 uu = User.create(email: 'email@email.com', username: 'user1', password: 123456)
 
-Room.create(link: 'http://link.com', level: 'begginer', language_id: ll, user_id: uu)
+Room.create(link: 'http://link.com', level: 'begginer', language: ll, user: uu)
