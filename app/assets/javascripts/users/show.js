802fe19c3e490f95fb81298272353eead49e3ca6
jonaselan <jonas.elan@gmail.com>
2017-10-14 09:28:41 -0300

    Adapt the share button behaviour

--- app/assets/javascripts/users/show.js
+++ app/assets/javascripts/users/show.js
@@ -1,16 +1,14 @@
 $(document).on('turbolinks:load', function() {
   if (!(page.controller() === 'users' && page.action() === 'show')) { return; }
-  console.log('ss');
-  
+
   var WIN_TOP = (screen.height / 2) - (350 / 2);
   var WIN_LEFT = (screen.width / 2) - (520 / 2);
 
-  function shareOnFacebook(link){
-    window.open("https://www.facebook.com/sharer/sharer.php?u="+ encodeURIComponent(link), "pop", "width=600, height=400, scrollbars=no, top="+WIN_TOP+", left="+ WIN_LEFT);
-    return false;
-  }
+  $('.btn-facebook').click(function() {
+    window.open("https://www.facebook.com/sharer/sharer.php?u="+ encodeURIComponent($(this).data('link')), "pop", "width=600, height=400, scrollbars=no, top="+WIN_TOP+", left="+ WIN_LEFT);
+  });
 
-  function shareOnTwitter(link){
+  $('.btn-twitter').click(function() {
     var text = "Replace this with your text";
     window.open('http://twitter.com/share?url='+encodeURIComponent(link)+'&text='+encodeURIComponent(text), '', 'left='+WIN_LEFT+',top='+WIN_TOP+',width=600,height=450,personalbar=0,toolbar=0,scrollbars=0');
   }
