93b8929f2657293de9813060e9d3ef76daa4151b
jonaselan <jonas.elan@gmail.com>
2017-10-17 10:16:12 -0300

    Improv flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,6 +6,16 @@ module ApplicationHelper
 
   def show_footer?(action, controller)
     (action == 'new' && controller == 'sessions') ||
-    (action == 'new' && controller == 'registrations')
+      (action == 'new' && controller == 'registrations')
+  end
+
+  def flash_message
+    messages = ''
+    %i[success info warning danger].each do |type|
+      if flash[type]
+        messages += "<p class=\"alert alert-#{type}\">#{flash[type]}</p>"
+      end
+    end
+    messages
   end
 end
