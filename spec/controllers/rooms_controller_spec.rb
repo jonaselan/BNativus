fb6a78a95a7165ca2edf24f428e7f1409aa9cd2b
jonaselan <jonas.elan@gmail.com>
2017-11-09 13:53:25 -0300

    Fix tests issue
    Add authentication on test environment, for get all access on controllers actions authenticate. Also will made some footer section improvments

--- spec/controllers/rooms_controller_spec.rb
+++ spec/controllers/rooms_controller_spec.rb
@@ -1,24 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe RoomsController, type: :controller do
-  let(:valid_attributes) {{ link: "MyString", level: "MyString" }}
+  let(:language) { create(:language) }
+  let(:valid_attributes) {{ link: "MyString", level: "MyString", language_id: language.id }}
   let(:invalid_attributes) { attributes_for(:room, link: '') }
 
-  # FIXME: ActionController::UrlGenerationError
   describe "POST #create" do
+    login_user
     context "with valid params" do
       it "creates a new Room" do
         expect {
-          post :create, params: {room: valid_attributes}
+          post :create, params: {room: valid_attributes, locale: I18n.locale }
         }.to change(Room, :count).by(1)
       end
       it "creates a new Room with ip related" do
-        post :create, params: {room: valid_attributes}
+        post :create, params: {room: valid_attributes, locale: I18n.locale }
         expect(Room.last.ip).to eq request.remote_ip
       end
       it "creates a new Room with user related" do
-        post :create, params: {room: valid_attributes}
-        expect(Room.last.user).to eq current_user
+        post :create, params: {room: valid_attributes, locale: I18n.locale }
+        expect(Room.last.user).to eq subject.current_user
+      end
+      it "increment a rooms number of current user" do
+        post :create, params: {room: valid_attributes, locale: I18n.locale }
+        expect(subject.current_user.created_rooms).to eq 1
       end
     end
   end
