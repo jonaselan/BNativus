49bdf5623e9cc8050fa3bbe1c5aedee85eabd4fb
jonaselan <jonas.elan@gmail.com>
2017-11-27 16:23:30 -0300

    Add test for model's permissions (ability)

---
+++ spec/models/ability_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+require "cancan/matchers"
+
+RSpec.describe Ability, type: :model do
+  subject(:ability){ Ability.new(user) }
+  let(:actions) { %i(show destroy edit) }
+  let(:user){ nil }
+
+  describe "abilities" do
+    describe "User" do
+      context "can only view, destroy and edit users which they own" do
+        let(:user){ create(:user) }
+        it{ is_expected.not_to be_able_to(actions, User.new) }
+      end
+    end
+
+    describe "Room" do
+      context "can only show, destroy and edit rooms which they own" do
+        let(:user){ create(:user) }
+        let(:room){ create(:room, user) }
+
+        it{ is_expected.not_to be_able_to(actions, Room.new) }
+      end
+    end
+
+    describe "Posting" do
+      context "can only show, destroy and edit postings which they own" do
+        let(:user){ create(:user) }
+        let(:posting){ create(:posting, user) }
+
+        it{ is_expected.not_to be_able_to(actions, Posting.new) }
+      end
+    end
+  end
+end
